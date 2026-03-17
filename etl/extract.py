b8507114ace6b0dad4fd4f70922182fcbb87c025
akrami <ramerame123890@gmail.com>
2026-03-17 19:23:03 +0200

    chore: make the extract_course function and its inner helper more robust, clear, and adhere to the DRY principle

--- etl/extract.py
+++ etl/extract.py
@@ -1,5 +1,6 @@
 import requests
 from etl.models.course_query import CourseQuery
+from etl.models.course_offering import  CourseOffering
 
 def extract_course(requested_course : CourseQuery):
         
@@ -9,14 +10,17 @@ def extract_course(requested_course : CourseQuery):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
-    def get(url):
-        response = requests.get(url, headers=headers, timeout=10)
+    def request_query(url):
+        query_params = "?$format=json&sap-language=EN"
+
+        response = requests.get(f"{url}{query_params}", headers=headers, timeout=10)
         response.raise_for_status()
-        return response.json()
+        return response.json()["d"]
 
     entity_url = f"{base_url}{requested_course}"
-    course_data = get(f"{entity_url}?$format=json")
-    prereq_data = get(f"{entity_url}/SmPrereq?$format=json")
+
+    course_data = request_query(f"{entity_url}")
+    prereq_data = request_query(f"{entity_url}/SmPrereq")
     
     return course_data, prereq_data
 
