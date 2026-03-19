4d019c3f38baf949d2b092eefb57f25681e7be5c
akrami <ramerame123890@gmail.com>
2026-03-19 15:41:41 +0200

    feat: add transform funcs for most of the data

---
+++ etl/transform.py
@@ -0,0 +1,37 @@
+def get_name(course_data):
+    return course_data["Name"]
+
+def get_syllabus(course_data):
+    return course_data["StudyContentDescription"]
+
+def get_points(course_data):
+    return int(course_data["PointsMaximum"])
+
+def get_course_id_8(course_data):
+    return course_data["Otjid"][2:]
+
+def get_course_id_6(course_data):
+    long_format = get_course_id_8(course_data)
+    lhs = long_format[1:3]
+    rhs = long_format[4:]
+    return lhs.join(rhs)
+
+def get_prereq_str(prereq_data):
+    results = prereq_data["results"]
+    
+    def is_module(entry):
+        return entry["ModuleId"] != "00000000"
+    
+    def get_course_str(entry):
+        if is_module:
+            return f"{entry["ModuleName"]}({entry["ModuleShort"]})"
+        return ""
+    
+    def format(entry):
+        return f"{entry["Bracket"]}{get_course_str(entry)}{entry["Operator"]}"
+    
+    prereq_str = ""
+    for entry in prereq_data:
+        prereq_str.join(format(entry))
+
+    return prereq_str
