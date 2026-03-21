fd0645323b6bb158d59ede7c9099bb5c62169f92
akrami <ramerame123890@gmail.com>
2026-03-21 10:22:07 +0200

    refactor: use the NavSegment Enum instead of hardcoding segments like before

--- etl/extract.py
+++ etl/extract.py
@@ -1,10 +1,9 @@
 import requests
 from etl.models.course_query import CourseQuery
-from etl.models.course_offering import  CourseOffering
+from etl.models.nav_segment import  NavSegment
 
-
-def request_query(requested_course : CourseQuery, section = None):
-    base_url = f"https://portalex.technion.ac.il/sap/opu/odata/sap/Z_CM_EV_CDIR_DATA_SRV/"
+def request_query(requested_course : CourseQuery, nav_segment : NavSegment = None):
+    base_url = "https://portalex.technion.ac.il/sap/opu/odata/sap/Z_CM_EV_CDIR_DATA_SRV/"
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -13,8 +12,8 @@ def request_query(requested_course : CourseQuery, section = None):
 
     entity_url = f"{base_url}{requested_course}"
 
-    if section:
-        entity_url += f"/{section}"
+    if nav_segment:
+        entity_url += f"{nav_segment}"
     
 
     query_params = "?$format=json&sap-language=EN"
@@ -28,7 +27,7 @@ def extract_course_general(requested_course : CourseQuery):
     return request_query(requested_course)
 
 def extract_course_prereqs(requested_course : CourseQuery):
-    return request_query(requested_course, "/SmPrereq")
+    return request_query(requested_course, NavSegment.PREREQ)
 
 
 
