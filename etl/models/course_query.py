49cc777dfce1945e65656922c0e902d1655766b4
akrami <ramerame123890@gmail.com>
2026-03-22 21:16:12 +0200

    feat: add an id validator function

--- etl/models/course_query.py
+++ etl/models/course_query.py
@@ -1,9 +1,21 @@
 class CourseQuery:
     def __init__(self, course_id, course_year, course_period):
-        self.course_id     = course_id 
+        self.course_id     = valid_id(course_id) 
         self.course_year   = course_year 
         self.course_period = course_period 
     
     def __str__(self):
         return f"SmObjectSet(Otjid='SM{self.course_id}',Peryr='{self.course_year}',Perid='{self.course_period}',ZzCgOtjid='',ZzPoVersion='',ZzScOtjid='')"
 
+
+def valid_id(id):
+    if len(id) > 8:
+        raise InvalidCourseID("Invalid ID, has more than 8 chars!!!")
+
+    if len(id) == 8:
+        return id
+    
+    while len(id) < 7:
+        id = '0' + id
+    
+    return id[0:4] + '0' + id[-3:]
