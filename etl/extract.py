f6853c7e59bc5865fbde7012cab96f0f43f72b8d
akrami <ramerame123890@gmail.com>
2026-03-23 20:28:50 +0200

    refactor(extract): refactor the heaeder to be more user/human-like, add sessions to seem more like a user, add prints for debugging/proggress indication

--- etl/extract.py
+++ etl/extract.py
@@ -2,14 +2,31 @@ import requests
 from etl.models.course_query import CourseQuery
 from etl.models.nav_segment import  NavSegment
 
+session = requests.Session()
+
+headers = {
+    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    "Sec-GPC": "1",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Origin": "https://portalex.technion.ac.il",
+    "Referer": "https://portalex.technion.ac.il/ovv/",
+    "X-Requested-With": "X",
+    "sap-contextid-accept": "header",
+    "sap-cancel-on-close": "true",
+    "Accept": "application/json",
+    "Accept-Language": "he",
+    "DataServiceVersion": "2.0",
+    "MaxDataServiceVersion": "2.0",
+}
+
+
 def request_query(requested_course : CourseQuery, nav_segment : NavSegment = None):
     base_url = "https://portalex.technion.ac.il/sap/opu/odata/sap/Z_CM_EV_CDIR_DATA_SRV/"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-
-
     entity_url = f"{base_url}{requested_course}"
 
     if nav_segment:
@@ -17,8 +34,11 @@ def request_query(requested_course : CourseQuery, nav_segment : NavSegment = Non
     
 
     query_params = "?$format=json&sap-language=EN"
+    full_url = f"{entity_url}{query_params}"
 
-    response = requests.get(f"{entity_url}{query_params}", headers=headers, timeout=10)
+    print(f"  GET {full_url}")
+    response = session.get(full_url, headers=headers, timeout=10)
+    print(f"  HTTP {response.status_code}")
     response.raise_for_status()
     return response.json()["d"]
 
