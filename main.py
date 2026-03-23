dbbe93ef26db7664e22b7c8dcf8f13022847ff3c
akrami <ramerame123890@gmail.com>
2026-03-23 20:34:21 +0200

    feat(prototype): add support for continuation of the ETL pipeline after crash or update or rerun and so on..

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def save(result_dict, output_path):
 
 def main():
     path_to_raw_input = "data/raw/course_ids.json"
+    output_path = 'res.json'
 
 
     year = '2025'
@@ -21,19 +22,25 @@ def main():
     with open(path_to_raw_input) as f:
         raw_input = json.load(f)
 
+    try:
+        with open(output_path) as f:
+            result_dict = json.load(f)
+        print(f"Resuming - {len(result_dict)} already done")
+    except FileNotFoundError:
+        result_dict = {}
+
+    already_done = result_dict.keys()
     current_query = CourseQuery('12345678', year, season)
 
     result_dict = {}
 
     try:
         for i,id in enumerate(raw_input['ids']):
-            print(i, id)
             current_query.set_course_id(id)
-            general_data     = extract_course_general(current_query)
-            # relations_data = extract_course_relations(current_query)
-            # reverse_data   = extract_course_relations_reverse(current_query)
-            # period_data    = extract_course_period(current_query)
-            # prereq_data    = extract_course_prereq(current_query)    
+            if current_query.course_id in already_done:
+                print(i, id, '- skip')
+                continue
+
 
             current_id       = get_course_id_8(general_data)
             current_syllabus = get_syllabus(general_data)
