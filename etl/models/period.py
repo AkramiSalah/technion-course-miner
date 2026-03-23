18426c8405a63081d97aba442ee95417450b7074
akrami <ramerame123890@gmail.com>
2026-03-23 20:24:06 +0200

    fix: correct Period Enum(off by one)

--- etl/models/period.py
+++ etl/models/period.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 class Period(Enum):
-    WINTER = 1
-    SPRING = 2
-    SUMMER = 3
+    WINTER = 0
+    SPRING = 1
+    SUMMER = 2
     
     def __str__(self):
         return f"20{self.value}"
